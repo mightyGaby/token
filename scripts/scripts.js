c97ed373e51b64dd71c610e059c9feae429ca8e1
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-06-07 22:45:23 -0500

    navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 
-$('#hero-sun').click(function(){
+$('.logo').click(function(){
   console.log('click')
   $(".hero").addClass('closed');
+  $(".nav").removeClass('closed');
+  // $(".nav .token-half").hide();
 });
